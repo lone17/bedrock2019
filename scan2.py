f30967b8c109de1e8dc671aba7fb40af6786fb26
lone17 <toilamduoctatca@gmail.com>
2019-08-11 13:00:44 +0700

    add table and checkbox

--- scan2.py
+++ scan2.py
@@ -1,4 +1,5 @@
 import pdfplumber
+import re
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,41 +7,49 @@ import matplotlib.pyplot as plt
 from utils import *
 
 # infile = "2019-20 ISFAA.pdf"
-infile = 'SOKA.pdf'
-with pdfplumber.open(infile) as pdf:
-    page = pdf.pages[2]
-    _, _, pdf_w, pdf_h = page.mediabox
+pdf_file = 'SOKA.pdf'
 
-    img = page.to_image(resolution=300)
-    
-    texts = page.extract_words(
-                               # x_tolerance=0, 
-                               y_tolerance=0
-                               )
-    texts = group_boxes(texts)
-    np_img = np.asarray(img.original)
-    img_h, img_w = np_img.shape[:2]
-    
-    texts = [{'x0': int(text['x0'] / pdf_w * img_w),
-              'x1': int(text['x1'] / pdf_w * img_w),
-              'y0': int(text['top'] / pdf_h * img_h),
-              'y1': int(text['bottom'] / pdf_h * img_h),
-              'value': text['text']} 
-             for text in texts]
-    
-    fields, remainng_texts = split_fields(texts, np_img)
-    for field in fields:
-        key_pos = field['key_pos']
-        field_pos = field['field_pos']
-        cv2.rectangle(np_img, tuple(key_pos[:2]), tuple(key_pos[2:]), (255,0,0), 5)
-        cv2.rectangle(np_img, tuple(field_pos[:2]), tuple(field_pos[2:]), (0,0,255), 5)
+def scan(pdf_file, page_idx):
+    with pdfplumber.open(pdf_file) as pdf:
+        page = pdf.pages[page_idx]
 
-    # img = img.draw_rects(page.rects, stroke=(0,255,0))
-    
-    # img = img.draw_lines(page.lines, stroke_width=4)
-    
-    # print(page.lines)
-    
-    plt.imshow(np_img)
-    plt.show()
-# img
+        img = page.to_image(resolution=300)
+        np_img = np.asarray(img.original)
+        
+        fields, remaining_texts = split_fields(page, np_img)
+        fields += detect_table_cells(page, np_img)
+        
+        # for field in fields:
+        #     # print(field)
+        #     key_pos = field.get('key_pos', None)
+        #     field_pos = field.get('field_pos', None)
+        #     if key_pos:
+        #         cv2.rectangle(np_img, tuple(key_pos[:2]), tuple(key_pos[2:]), (255,0,0), 5)
+        #     if field_pos:
+        #         cv2.rectangle(np_img, tuple(field_pos[:2]), tuple(field_pos[2:]), (0,0,255), 5)
+
+        # cells = []
+        # table_settings = {
+        #     'horizontal_strategy': 'lines',
+        #     'vertical_strategy': 'lines',
+        #     'edge_min_length': 100,
+        # }
+        # for table in page.find_tables(table_settings):
+        #     cells += table.cells
+        # img = img.draw_rects(cells, stroke=(0,255,0), stroke_width=5)
+        # img = img.draw_rects(texts, stroke=(0,255,0), stroke_width=11)
+        
+        # img = img.draw_lines(page.lines, stroke_width=4)
+        
+        # print(page.lines)
+        
+        # plt.imshow(np_img)
+        # plt.show()
+        
+        fields = format_fields(fields, np_img)
+        
+        return fields
+
+if __name__ == '__main__':
+    for f in scan(pdf_file, 5):
+        print(f)
--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
-from PIL import Image
-import numpy as np
+import re
+
 import cv2
+import numpy as np
+from PIL import Image
 from matplotlib import pyplot as plt
 
 def merge_boxes(boxes):
@@ -71,7 +73,7 @@ def group_boxes(boxes, thresh=0):
     
     return new_boxes
 
-def detect_dash(line_img):
+def detect_dashes(line_img):
     # plt.imshow(line_img)
     # plt.show()
     if len(line_img.shape) > 2:
@@ -116,11 +118,27 @@ def detect_dash(line_img):
 
     return max(lines, key=lambda l: l[2] - l[0])
 
-def split_fields(texts, np_img):
-    box = '☐'
+def split_fields(page, np_img):
+    _, _, pdf_w, pdf_h = page.mediabox
+    img_h, img_w = np_img.shape[:2]
+    
+    texts = page.extract_words(
+                               # x_tolerance=0, 
+                               y_tolerance=0
+                               )
+    texts = group_boxes(texts)
+    
+    texts = [{'x0': int(text['x0'] / pdf_w * img_w),
+              'x1': int(text['x1'] / pdf_w * img_w),
+              'y0': int(text['top'] / pdf_h * img_h),
+              'y1': int(text['bottom'] / pdf_h * img_h),
+              'value': text['text']} 
+             for text in texts if text['top'] / pdf_h < 0.9]
+    
+    checkbox = r'☐'
     dash = '_'
 
-    remainng_texts = []
+    remaining_texts = []
     fields = []
     
     for text in texts:
@@ -128,49 +146,149 @@ def split_fields(texts, np_img):
         x1 = text['x1']
         y0 = text['y0']
         y1 = text['y1']
+        remain = True
         if dash * 5 in text['value']:
-            key = text['value'].replace('_', '').strip()
+            key = text['value'].replace(dash, '').strip()
             if key != '':
-                line = detect_dash(np_img[y0:y1, x0:x1])
+                line = detect_dashes(np_img[y0:y1, x0:x1])
                 field_pos = [line[0] + x0, y0, line[2] + x0, y1] 
                 fields.append({'key': key,
                                'key_pos': [x0, y0, field_pos[0], y1],
                                'field_pos': field_pos,
                                'type': 'string'})
+                remain = False
             else:
-                remainng_texts.append(text)
-        else:
-            remainng_texts.append(text)
-            
-    return fields, remainng_texts
+                fields.append({'field_pos': [x0, y0, x1, y1],
+                               'type': 'string'})
+                remain = False
+        elif bool(re.search(checkbox, text['value'])):
+            # print(text['value'])
+            key = re.sub(checkbox, '', text['value']).strip()
+            if key != '':
+                squares = detect_squares(np_img[y0:y1, x0:x1])
+                if len(squares) == 1:
+                    # print(squares)
+                    square = squares[0]
+                    checkbox_pos = [square[0] + x0, square[1] + y0, 
+                                    square[2] + x0, square[3] + y0]
+                    if checkbox_pos[0] - x0 > x1 - checkbox_pos[2]:
+                        key_pos = [x0, y0, checkbox_pos[0], y1]
+                    else:
+                        key_pos = [checkbox_pos[2], y0, x1, y1]
+                    fields.append({'key': key,
+                                   'key_pos': key_pos,
+                                   'field_pos': checkbox_pos,
+                                   'type': 'checkbox'})
+                    remain = False
+            else:
+                # print(text['value'])
+                fields.append({'field_pos': [x0, y0, x1, y1],
+                               'type': 'checkbox'})
+                remain = False
+        
+        if remain:
+            remaining_texts.append(text)
+
+    return fields, remaining_texts
 
 
 def detect_squares(img):
-    
+    # plt.imshow(img)
+    # plt.show()
     def angle_cos(p0, p1, p2):
-        d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
-        return abs( np.dot(d1, d2) / np.sqrt( np.dot(d1, d1)*np.dot(d2, d2) ) )
+        d1, d2 = (p0 - p1).astype('float'), (p2 - p1).astype('float')
+        return abs(np.dot(d1, d2) / np.sqrt(np.dot(d1, d1)*np.dot(d2, d2)))
     
     img = cv2.GaussianBlur(img, (5, 5), 0)
     squares = []
     for gray in cv2.split(img):
-        for thrs in xrange(0, 255, 26):
+        for thrs in range(0, 255, 26):
             if thrs == 0:
                 bin = cv2.Canny(gray, 0, 50, apertureSize=5)
                 bin = cv2.dilate(bin, None)
             else:
                 _retval, bin = cv2.threshold(gray, thrs, 255, cv2.THRESH_BINARY)
-            bin, contours, _hierarchy = cv2.findContours(bin, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-            for cnt in contours:
+            bin, contours, _hierarchy = \
+                cv2.findContours(bin, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            for cnt in contours[1:]:
                 cnt_len = cv2.arcLength(cnt, True)
                 cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
-                if len(cnt) == 4 and cv2.contourArea(cnt) > 1000 and cv2.isContourConvex(cnt):
+                if (len(cnt) == 4 
+                    and cv2.contourArea(cnt) > 1000 
+                    and cv2.isContourConvex(cnt)):
                     cnt = cnt.reshape(-1, 2)
-                    max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in xrange(4)])
+                    max_cos = np.max([angle_cos(cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4]) 
+                                      for i in range(4)])
                     if max_cos < 0.1:
-                        squares.append(cnt)
+                        # squares.append(cnt)
+                        (x,y,w,h) = cv2.boundingRect(cnt)
+                        if [x, y, x+w, y+h] not in squares:
+                            squares.append([x, y, x+w, y+h])
     
-    cv.drawContours(img, squares, -1, (0, 255, 0), 3 )
-    plt.imshow(img)
-    plt.show()
+    # cv2.drawContours(img, squares, -1, (0, 255, 0), 3 )
+    # print(squares)
+    # for box in squares:
+    #     print(box)
+    #     cv2.rectangle(img, tuple(box[:2]), tuple(box[2:]), (255,0,0), 1)
+    # plt.imshow(img)
+    # plt.show()
+    # if len(squares) > 1:
+    #     return squares[1:]
     return squares
+
+def detect_table_cells(page, np_img):
+    _, _, pdf_w, pdf_h = page.mediabox
+    # print(pdf_h, pdf_w)
+    img_h, img_w = np_img.shape[:2]
+    
+    fields = []
+
+    table_settings = {
+        'horizontal_strategy': 'lines',
+        'vertical_strategy': 'lines',
+        'edge_min_length': 100,
+        'intersection_tolerance': 1,
+        # 'text_y_tolerance':5,
+        # 'text_x_tolerance': 5,
+        'snap_tolerance': 7,
+        'join_tolerance': 3,
+    }
+    for table in page.find_tables(table_settings):
+        # print(table.bbox)
+        _, _, table_w, table_h = table.bbox
+        for cell in table.cells:
+            texts = page.within_bbox(cell).extract_words()
+            # print(texts)
+            if len(texts) == 0:
+                # print('\t', cell)
+                field_pos = [
+                             int(cell[0] / pdf_w * img_w),
+                             int(cell[1] / pdf_h * img_h),
+                             int(cell[2] / pdf_w * img_w),
+                             int(cell[3] / pdf_h * img_h),
+                             ]
+                # print(field_pos)
+                fields.append({'field_pos': field_pos, 'type': 'string'})
+                
+    # print(len(fields))
+    return fields
+
+def format_fields(fields, np_img):
+    img_h, img_w = np_img.shape[:2]
+    formatted = []
+    for f in fields:
+        x0, y0, x1, y1 = f['field_pos']
+        item = {
+            'x0': x0 / img_w,
+            'x1': x1 / img_w,
+            'y0': y0 / img_h,
+            'y1': y1 / img_h,
+            'type': f['type'],
+            'key': {
+                   'name': f.get('key', None),
+                   'pos': f.get('key_pos', None),
+            }
+        }
+        formatted.append(item)
+    
+    return formatted
